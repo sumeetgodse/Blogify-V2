d5f67439d46db34e1a16120c2d8ac80c6a69ca8b
Sumeet Godse <sumeetgodse1997@gmail.com>
2021-05-04 15:24:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: AIzaSyBa8S7WpKOXwNv0VQuWpnQOIzbWEhKIcII,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
